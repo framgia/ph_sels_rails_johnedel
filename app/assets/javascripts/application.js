4161552fbd912d3c86a3f2a218a59bceb10effef
Johnedel S. Mapa <34313943+johnedelmapa@users.noreply.github.com>
2019-07-17 15:21:58 +0800

    Create Login (#5)
    * Create Login
    
    * Fixed jquery issue

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,12 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require materialize
+//= require materialize/extras/nouislider
+//= require jquery  
+//= require turbolinks  
+//= require materialize-sprockets
 //= require rails-ujs
 //= require activestorage
-//= require turbolinks
-//= require_tree .
+//= require_tree
+
 
