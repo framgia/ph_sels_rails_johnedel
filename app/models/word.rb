98551325391ea8663ff75e05141eb49954ce424c
Johnedel S. Mapa <34313943+johnedelmapa@users.noreply.github.com>
2019-07-25 10:35:41 +0800

    Polish Administration (#13)
    * Polish Administration
    
    * Correct changed to boolean

--- app/models/word.rb
+++ app/models/word.rb
@@ -2,13 +2,27 @@ class Word < ApplicationRecord
   belongs_to :category
   has_many :choices, dependent: :destroy
   accepts_nested_attributes_for :choices, allow_destroy: true,  reject_if: :all_blank
-  validates :content, presence: true, length: { maximum: 50 }
+  validates :content, length: { maximum: 50 }
+  validates_presence_of :content, message: "word can't be blank"
   default_scope -> { order(created_at: :desc) }
+  validate :one_choice_only
+  validate :must_unique
  
-  
   def correct_ans
     choices.find_by(correct: true).try(:content)
   end
 
+  def one_choice_only
+    answer = choices.collect { |choice| choice.correct}.count(true)
+    unless answer == 1
+      return errors.add(:base, "Please check only one correct answer")
+    end
+  end
 
+  def must_unique
+    uniqLength = choices.uniq { |choice| choice.content.downcase }.count
+    if uniqLength < choices.length
+      return errors.add(:base, "Duplication of choices, it must be unique")
+    end
+  end
 end
