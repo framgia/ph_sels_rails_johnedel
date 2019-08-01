b3f18f0809e23b053380e1871c5e7c706dcf099f
johnedelmapa <johnedel.mapa@sun-asterisk.com>
2019-08-01 14:20:02 +0800

    Improvements polish UI

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,7 +23,7 @@ class User < ApplicationRecord
                     uniqueness: { case_sensitive: false }    
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
-
+  
   def follow(other_user)
     following << other_user
   end
